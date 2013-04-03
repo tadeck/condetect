3ae467f0df03eb8f71c1ae845e5db398c6f449e7
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2013-04-03 23:51:04 +0200

    initial commit; support for isSPDY() and isSSL() on Chrome and Firefox

---
+++ README.rst
@@ -0,0 +1,104 @@
+=====================
+``condetect`` Library
+=====================
+
+This library is small utility originally designed for connection-related
+detections. Consider it a shorthand for protocol checks and connection-related
+tasks.
+
+The library is in development stage, so any feedback is encouraged. Please do
+not hesitate to use it - any issues should be reported through GitHub issues
+page.
+
+Changelog
+=========
+
++---------+------------+------------------------------------------------------+
+| Version | Date       | Changes                                              |
++=========+============+======================================================+
+| 0.0.1   | 2013-04-03 | - initial release,                                   |
+|         |            | - support for detection of SPDY availability,        |
++---------+------------+------------------------------------------------------+
+
+Features
+========
+
+The following features are available:
+
+- detect if current connection uses SPDY protocol,
+
+The following features are planned:
+
+- connection features detection (eg. connection through SSL, compression),
+- connectivity issues detection,
+- ability to work with separate AJAX calls or by intercepting existing ones,
+- configurable endpoints for AJAX tests,
+
+How to use the library
+======================
+
+When loaded, the library creates global object called ``condetect`` (as the
+name off the library, abbreviation of "*connection detect*"). This object has
+some useful properties, such as:
+
+- ``condetect.isSSL()`` - returns ``true`` if connection is done through SSL,
+  ``false`` if connection is not done through SSL, and returns ``undefined``,
+  when the protocol cannot be identified,
+- ``condetect.isSPDY()`` - returns ``true`` if connection is done through SPDY,
+  ``false`` when not, and ``undefined`` when the library cannot distinguish it,
+
+Both ``condetect.isSSL()`` and ``condetect.isSPDY()`` accept window object as
+an argument, if you want to check some specific window. If you have
+insufficient permissions for specific window (eg. for ``iframe`` from external
+site), do not be surprised with ``undefined`` returned by the above calls.
+
+Example no. 1 - redirect to SSL
+-------------------------------
+
+To redirect to secure connection you can do the following::
+
+    function secureLocation(oldLocation){
+        // build location by changing the protocol
+        return 'https://' + oldLocation.hostname + oldLocation.pathname;
+    }
+
+    if (!condetect.isSSL()){
+        // connection is not secure, redirect to secure location
+        window.location = secureLocation(window.location);
+    }
+
+Example no. 2 - switch for SPDY
+-------------------------------
+
+Since you are able to detect SPDY on some browsers, you may wish to let user
+know, that he is using some pretty cool technology::
+
+    if (condetect.isSPDY()){
+        // user is connecting through SPDY, let him know
+        alert('You are using faster connection version. Congratulations!');
+    }
+
+Of course, you can apply similar checks for more advanced features provided by
+SPDY protocol.
+
+Browser compatibility
+=====================
+
+Right now the compatibility with older (deprecated) browsers is not planned,
+but further tests should reveal the degree of library's compatibility with such
+browsers. Any test results giving some insight into this subject are
+encouraged.
+
+The module has been tested so far with:
+
+- Google Chrome 25,
+- Mozilla Firefox 18,
+
+License
+=======
+
+The license has not been chosen yet. You can use the library for free, modify
+it and distribute. However, please include the reference to the original
+library or its author. The library is provided free of charge and without any
+guarantee, of any kind. In return please consider giving feedback on it, eg.
+testing it for different browsers and providing the results and/or fixes.
---
+++ src/condetect.js
@@ -0,0 +1,152 @@
+/**
+ * condetect 0.0.1-alpha library
+ * @version 0.0.1-alpha
+ * @author Tomasz Jaskowski (https://github.com/tadeck)
+ */
+(function (window) {
+    "use strict";
+    var that = {}, // main container object
+        api = {}; // external interface
+
+    /**
+     * Log message to the console
+     * @param msg
+     */
+    that.log = function (msg) {
+        if (window.console !== undefined && window.console.log !== undefined) {
+            window.console.log(msg);
+        }
+    };
+
+    /**
+     * Indicate there was some kind of error. Such cases should be eliminated
+     * during the development (this call is mostly for debugging problems).
+     * @param msg the message to be displayed as error
+     */
+    that.error = function (msg) {
+        that.log('ERROR: ' + msg); // just pass it to log for now
+    };
+
+    /**
+     * Allows external dependency injection (use only for deep testing)
+     * @param name name of the method
+     * @param new_method new method to override current one
+     */
+    that.overrideMethod = function (name, new_method) {
+        that[name] = new_method;
+    };
+
+    /**
+     * Check if AJAX request to specific URL shows specific SPDY request
+     * @param location URL to call with AJAX request
+     * @returns {boolean|undefined}
+     */
+    that.ajaxCallUsesSPDY = function (location) {
+        var req, result, spdy_header;
+        req = new window.XMLHttpRequest();
+        req.open('GET', location, false); // synchronous check
+        req.send(null);
+        // making synchronous call, so no need for event-based approach
+        spdy_header = req.getResponseHeader('X-Firefox-Spdy');
+        if (spdy_header === null) {
+            result = false;
+        } else {
+            // header found and is text, check if is bigger than "1"
+            result = spdy_header > 1;
+        }
+        return result;
+    };
+
+    /**
+     * Get location object associated with given window
+     * @param window_obj window object
+     * @returns {boolean|undefined}
+     */
+    that.getLocation = function (window_obj) {
+        if (window_obj === undefined) {
+            window_obj = window;
+        }
+        var result;
+        if (window_obj.location !== undefined) {
+            result = window_obj.location;
+        } else {
+            result = window_obj.document.location;
+        }
+        return result;
+    };
+
+    /**
+     * Check if connection for specific window was invoked using SPDY protocol
+     * @param window_obj window which needs to be checked
+     * @returns {boolean|undefined} true (yes), false (no), undefined (unknown)
+     */
+    that.isSPDY = function (window_obj) {
+        if (window_obj === undefined) {
+            window_obj = window;
+        }
+        var result;
+        if (window_obj.chrome !== undefined &&
+                window_obj.chrome.loadTimes !== undefined) {
+            // we are dealing with Chrome-like browser
+            result = window_obj.chrome.loadTimes().wasFetchedViaSpdy;
+        } else {
+            result = that.ajaxCallUsesSPDY(that.getLocation(window_obj));
+        }
+        that.log('Check for SPDY shows: ' + result);
+        return result;
+    };
+
+    /**
+     * Check if connection for specific window was invoked using SSL.
+     * @param window_obj window to check
+     * @returns {boolean|undefined}
+     */
+    that.isSSL = function (window_obj) {
+        if (window_obj === undefined) {
+            window_obj = window;
+        }
+        var location, result;
+        location = that.getLocation(window_obj);
+        if (location.protocol !== undefined) {
+            result = that.protocolIsSecure(location.protocol);
+        } else {
+            that.error('cannot retrieve protocol');
+            result = undefined;
+        }
+        return result;
+    };
+
+    /**
+     * Check if passed protocol is secure. May return undefined, if cannot
+     * decide either way.
+     * @param protocol the string contained in eg. location.protocol
+     * @returns {boolean|undefined}
+     */
+    that.protocolIsSecure = function (protocol) {
+        var patterns, result;
+        patterns = {
+            'secure': /^https:/,
+            'insecure': /^(http|file|ftp):/
+        };
+        if (patterns.secure.test(protocol)) {
+            result = true;
+        } else if (patterns.insecure.test(protocol)) {
+            result = false;
+        } else {
+            that.error(
+                'encountered unknown protocol: ' + protocol
+            );
+            result = undefined;
+        }
+        return result;
+    };
+
+    // publish external interface:
+    api.isSPDY = that.isSPDY;
+    api.isSSL = that.isSSL;
+
+    // deep interference with library:
+    api.overrideMethod = that.overrideMethod;
+
+    window.condetect = api;
+}(window));
